3c5ae335547c6909f47a4a8cf502ef16ef2c84f8
aidenZou <aidenzou@gmail.com>
2015-12-22 01:02:11 +0800

    add pro

---
+++ app/src/main/java/com/example/aidenzou/test/myapplication/MainActivity.java
@@ -0,0 +1,275 @@
+package com.example.aidenzou.test.myapplication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String url = "https://youcai.shequcun.com/#!/";
+    //    private String url = "http://m.baidu.com";
+    private WebView webView;
+    private Toolbar toolbar;
+
+    private ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        init();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent();
+            intent.setAction("aaa.bbb.settings");
+            intent.addCategory("android.intent.category.DEFAULT");
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_share) {
+            Toast.makeText(getApplicationContext(), "分享未实现", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.action_youcai) {
+            // 通过浏览器打开
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } else if (id == R.id.action_refresh) {
+            webView.reload();
+        } else if (id == R.id.action_show_url) {
+            Toast.makeText(this, webView.getUrl(), Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_login) {
+            Intent intent = new Intent();
+            intent.setAction("aaa.bbb.login");
+            intent.addCategory("android.intent.category.DEFAULT");
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void init() {
+        webView = (WebView) findViewById(R.id.webView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // 配置调试WebViews
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        WebSettings webSettings = webView.getSettings();
+        // 启用支持 Javascript
+        webSettings.setJavaScriptEnabled(true);
+        // 有限使用缓存
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 不使用缓存
+        //webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        // localStorage
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheMaxSize(1024 * 1024 * 8);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        webSettings.setAppCachePath(appCachePath);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
+
+        webView.loadUrl(url);
+
+        // WebViewClient帮助 WebView去处理一些页面控制 和 请求通知
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // 阻止通过默认浏览器打开网页,此方法可以在webview中打开链接而不会跳转到外部浏览器
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            // 网页加载完
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 在这里执行你想调用的js函数
+                Toast.makeText(MainActivity.this, "页面加载完毕", Toast.LENGTH_SHORT).show();
+            }
+
+            // WebView 错误码处理
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+
+                view.loadUrl("file:///android_asset/error.html");
+            }
+
+            // 处理https请求，为WebView处理ssl证书设置
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();  // 接受信任所有网站的证书
+                // handler.cancel();   // 默认操作 不处理
+                // handler.handleMessage(null);  // 可做其他处理
+                //super.onReceivedSslError(view, handler, error);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                // 设置title
+                toolbar.setTitle(title);
+                super.onReceivedTitle(view, title);
+            }
+
+            // 实现进度条
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                // newProgress 1-100 之间的整数
+                if (newProgress == 100) {
+                    // 网页加载完毕
+                    closeDialog();
+                } else {
+                    // 网页正在加载
+                    openDialog(newProgress);
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+
+            private void openDialog(int newProgress) {
+                if (dialog == null) {
+                    dialog = new ProgressDialog(MainActivity.this);
+                    dialog.setTitle("正在加载");
+                    dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                    dialog.setProgress(newProgress);
+                    dialog.show();
+                } else {
+                    dialog.setProgress(newProgress);
+                }
+            }
+
+            private void closeDialog() {
+                if (dialog != null && dialog.isShowing()) {
+                    dialog.dismiss();
+                    dialog = null;
+                }
+            }
+
+        });
+
+        // 屏蔽掉长按事件 因为webview长按时将会调用系统的复制控件
+        webView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+
+    // 按键事件处理
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            } else {
+                // 退出程序
+                System.exit(0);
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
